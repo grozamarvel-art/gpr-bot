5cc2b1ca77302e1efc945811f71a8442ca738e88
grozamarvel-art <grozamarvel@gmail.com>
2026-02-11 14:27:24 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,24 @@
+import asyncio
+import os
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from aiogram.filters import CommandStart
+
+BOT_TOKEN = os.getenv("8593187949:AAEIt1yRfAwNctcc4uSUh2VpizaovD9lQuc")
+
+if not BOT_TOKEN:
+    raise ValueError("❌ Ошибка: Не найден токен бота!")
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer("✅ Бот ГПР запущен!\nГотов принимать отчёты.")
+
+async def main():
+    print("🚀 Бот запущен! Напишите /start в Telegram")
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
